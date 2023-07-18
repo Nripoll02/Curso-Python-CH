5142a0053e5c65e0a03cd61414bcbd6dcb605769
Nahuel <nahuelripoll33@gmail.com>
2023-07-18 20:14:49 -0300

    Curso Python Coderhouse

---
+++ main.py
@@ -0,0 +1,12 @@
+#Sumar todos los nuemeros del 1 al 100
+
+# Inicializar la variable de suma
+suma = 0
+
+# Iterar sobre todos los números del 1 al 100
+for numero in range(1, 101):
+    # Sumar cada número a la variable de suma
+    suma += numero
+
+# Imprimir el resultado
+print("La suma de todos los números hasta el 100 es:", suma)
